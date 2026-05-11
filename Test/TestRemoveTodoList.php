7d0a38d1d996d61e233e183a51aa830bc5f8f655
unknown <feryrichardo19@gmail.com>
2026-05-11 19:15:53 +0700

    new clean architecture

--- Test/TestRemoveTodoList.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once 'Model/TodoList.php';
-require_once '../BussinessLogic/RemoveTodoList.php';
-
--- View/TodolistView.php
+++ View/TodolistView.php
@@ -19,7 +19,7 @@ namespace View {
                 echo "Menu: \n";
                 echo "1. Tambah\n";
                 echo "2. Hapus\n";
-                echo "3. Keluar\n";
+                echo "x. Keluar\n";
 
                 $pilihan = InputHelper::input("Pilih");
 
@@ -27,7 +27,7 @@ namespace View {
                     $this->addTodoList();
                 } else if ($pilihan == "2") {
                     $this->removeTodoList();
-                } else if ($pilihan == "3") {
+                } else if ($pilihan == "x") {
                     break;
                 } else {
                     echo "Pilihan tidak dimengerti\n";
@@ -36,11 +36,32 @@ namespace View {
         }
 
         function addTodoList(): void {
+            echo "Menambah Todo\n";
 
+            $todo = InputHelper::input("Todo (Tekan 'x' untuk batal) ");
+
+            if ($todo == "x") {
+                echo "Batal menambah todo\n";
+            } else {
+                $this->todolistService->addTodoList($todo);
+            }
         }
 
         function removeTodoList(): void {
-            
+            echo "Menghapus Todo\n";
+
+            $pilihan = InputHelper::input("Nomor todo yang akan dihapus (Tekan 'x' untuk batal) ");
+
+            if ($pilihan == "x") {
+                echo "Batal menghapus todo\n";
+                echo "Terima kasih\n";
+            } else {
+                $success = $this->todolistService->removeTodoList($pilihan);
+
+                if (!$success) {
+                    echo "Gagal menghapus todo, nomor tidak ditemukan\n";
+                }
+            }
         }
     }
     
